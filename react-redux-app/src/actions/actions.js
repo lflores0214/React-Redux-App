2bdcd41b1f43ffe3c950226b37afc1252f67d61b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-11-07 20:14:29 -0700

    fixed error. now able to fetch data and display it. added styling

--- react-redux-app/src/actions/actions.js
+++ react-redux-app/src/actions/actions.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 export const FETCH_AVATAR_LOADING = "FETCH_AVATAR_LOADING";
 export const FETCH_AVATAR_SUCCESS = "FETCH_AVATAR_SUCCESS";
 export const FETCH_AVATAR_FAILURE = "FETCH_AVATAR_FAILURE";
 
 export const avatarLoading = () => ({ type: FETCH_AVATAR_LOADING });
-export const avatarSuccess = () => ({
+export const avatarSuccess = data => ({
   type: FETCH_AVATAR_SUCCESS,
   payload: data
 });
-export const avatarFailure = () => ({
+export const avatarFailure = error => ({
   type: FETCH_AVATAR_FAILURE,
   payload: error
 });
@@ -17,14 +17,12 @@ export const avatarFailure = () => ({
 export const getAvatar = () => {
   return function(dispatch) {
     dispatch(avatarLoading());
-  };
-  return axios
-  .get(`https://last-airbender-api.herokuapp.com/api/v1/characters`)
-  .then(response => {
-      console.log(response)
-      dispatch(avatarSuccess(response))
-  })
-  .catch
-  (error => disaptch(avatarFailure(error)))
 
+    return axios
+      .get(`https://last-airbender-api.herokuapp.com/api/v1/characters/`)
+      // .then(response => console.log(response.data
+      //   ))
+      .then(response => dispatch(avatarSuccess(response.data)))
+      .catch(error => dispatch(avatarFailure(error)));
+  };
 };
